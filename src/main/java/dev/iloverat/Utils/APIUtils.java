a7a6fcf4027e5c585c0ba9db0f0ee0f44d21e288
Dev381 <49997896+Dev380@users.noreply.github.com>
2023-03-20 20:11:21 -0400

    Add method to convert oauth tokens to minecraft

--- src/main/java/dev/iloverat/Utils/APIUtils.java
+++ src/main/java/dev/iloverat/Utils/APIUtils.java
@@ -26,6 +26,19 @@ public class APIUtils {
         String UUID = jsonObject.get("id").getAsString();
         return new String[]{IGN, UUID};
     }
+    public static String[] tokenFromMicrosoft(String code) throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost request = new HttpPost("https://api.minecraftservices.com/authentication/login_with_xbox");
+        request.setHeader("Content-Type", "application/json");
+        String jsonString = String.format("{ \"identityToken\": \"XBL3.0 x=%s;\"}", code);
+        request.setEntity(new StringEntity(jsonString));
+        CloseableHttpResponse response = client.execute(request);
+        jsonString = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+        jsonObject = new JsonParser().parse(jsonString).getAsJsonObject();
+        String accessToken = jsonObject.get("access_token").getAsString();
+        String clientToken = jsonObject.get("client_token").getAsString();
+        return new String[]{accessToken, clientToken};
+    }
     public static Boolean validateSession(String token) throws IOException {
         try {
             String[] profileInfo = getProfileInfo(token);
